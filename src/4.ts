a413c4abe7126095d8e11be3833c9d4d67d0844a
Oleksandr-Spd <133060544+Oleksandr-Spd@users.noreply.github.com>
2024-03-16 01:51:12 +0200

    finish

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,63 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(door: boolean, key: Key) {
+    this.door = door;
+    this.key = key;
+  }
+
+  abstract OpenDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person entered the house.");
+    } else {
+      console.log("The door is closed");
+    }
+  }
+}
+
+class MyHouse extends House {
+  OpenDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is now open.");
+    } else {
+      console.log("Invalid key.");
+    }
+  }
+}
+
 const key = new Key();
 
-const house = new MyHouse(key);
+const house = new MyHouse(false, key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.OpenDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
